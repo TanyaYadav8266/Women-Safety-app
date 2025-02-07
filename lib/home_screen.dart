a76df0d0b8464b8e0688e53c691463d1be0dbc7c
tanya8266 <tanya8266y@gmail.com>
2025-02-07 11:35:05 +0530

    first commit in this repo

---
+++ lib/home_screen.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:title_proj/widgets/CustomCarouel.dart';
+import 'package:title_proj/widgets/custom_appBar.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  //const HomeScreen({super.key});
+  int qIndex = 2;
+
+  getRandomQuote(){
+    Random random = Random ();
+    setState(() {
+      qIndex = random.nextInt(6);
+    });
+  }
+  @override
+  void initState() {
+    getRandomQuote();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body:SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children:[
+              CustomAppBar(
+                quoteIndex: qIndex,
+                onTap: (){
+                  getRandomQuote();
+                }),
+              CustomCarouel(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/quotes.dart
@@ -0,0 +1,21 @@
+List sweetSayings = [
+  "There is no limit to what we, as women, can accomplish.",
+  "A woman is the full circle. Within her is the power to create, nurture, and transform.",
+  "She was powerful not because she wasn’t scared, but because she went on so strongly, despite the fear.",
+  "I am grateful to be a woman. I must have done something great in another life.",
+  "The question isn’t who’s going to let me; it’s who is going to stop me.",
+  "Well help you in the way we can",
+];
+
+List<String> articleTitle = [
+  "Strength, courage, and power define you",
+  "Lead with grace, rise with power",
+  "Fearless, bold, unstoppable – that’s you",
+  "Your voice matters, make it heard"
+];
+List<String> imageSliders = [
+  "https://media.istockphoto.com/photos/silhouette-of-super-strong-successful-businesswoman-picture-id1249879109?k=20&m=1249879109&s=612x612&w=0&h=I-joEMjqkq1wCJZJeLWUCd1d2HcB5WxBShdkA9YM0cg=",
+  "https://media.istockphoto.com/vectors/young-woman-looks-at-the-mirror-and-sees-her-happy-reflection-vector-id1278815846?k=20&m=1278815846&s=612x612&w=0&h=JUTmV9Of-_ILOfXBfV9Cmp_41yuTliSdFIcZy5LKuss=",
+  "https://media.istockphoto.com/vectors/mental-health-or-psychology-concept-with-flowering-human-head-vector-id1268669581?k=20&m=1268669581&s=612x612&w=0&h=YVLTKCZXKugEn40aqOkir4vcoFeTUAQToa1i3AFYRNU=",
+  "https://media.istockphoto.com/photos/confidence-and-strength-concept-picture-id1086700012?k=20&m=1086700012&s=612x612&w=0&h=1wWVN3AB7BH7o3y2A2b-NG3HB9H6Dwkc9OLz2lxgwAY=",
+];
